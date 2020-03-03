a03c8dc4f5d0974f769a97cf0a7e6d6b9c774d35
chetan <chetankudaravalli16@gmail.com>
2020-03-02 22:56:10 -0600

    worked on customer registration page, about fragment, worker ful details page

---
+++ app/src/main/java/com/example/getaservice/CustomerRegisterActivity.java
@@ -0,0 +1,99 @@
+package com.example.getaservice;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CustomerRegisterActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button btnSignUp;
+    private ProgressDialog PD;
+    private TextView  btnLogin;
+
+
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_customer);
+
+        PD = new ProgressDialog(this);
+        PD.setMessage("Loading...");
+        PD.setCancelable(true);
+        PD.setCanceledOnTouchOutside(false);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(CustomerRegisterActivity.this, MainActivity.class));
+            finish();
+        }
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        btnLogin = (TextView) findViewById(R.id.sign_in_button);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+                final String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                try {
+                    if (password.length() > 0 && email.length() > 0) {
+                        PD.show();
+                        auth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(CustomerRegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (!task.isSuccessful()) {
+                                            Toast.makeText(
+                                                    CustomerRegisterActivity.this,
+                                                    "Authentication Failed",
+                                                    Toast.LENGTH_LONG).show();
+                                            Log.v("error", task.getResult().toString());
+                                        } else {
+                                            Intent intent = new Intent(CustomerRegisterActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        PD.dismiss();
+                                    }
+                                });
+                    } else {
+                        Toast.makeText(
+                                CustomerRegisterActivity.this,
+                                "Fill All Fields",
+                                Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/getaservice/WorkerFullDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.getaservice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WorkerFullDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workerdetails);
+        {
+
+
+        }
+    }
+}
