9f3a7d241427de7276b3d1b241dbcf8bd695e398
mohanpratapa <pvskrishnamohan24@gmail.com>
2020-03-02 22:31:14 -0600

    i worked on send fragment and LoginActivity

---
+++ app/src/main/java/com/example/getaservice/LoginActivity.java
@@ -0,0 +1,114 @@
+package com.example.getaservice;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button  btnLogin;
+    private ProgressDialog PD;
+    private TextView btnSignUp;
+
+    @Override    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        PD = new ProgressDialog(this);
+        PD.setMessage("Loading...");
+        PD.setCancelable(true);
+        PD.setCanceledOnTouchOutside(false);
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnSignUp = (TextView) findViewById(R.id.sign_up_button);
+        btnLogin = (Button) findViewById(R.id.sign_in_button);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override            public void onClick(View view) {
+//                final String email = inputEmail.getText().toString();
+//                final String password = inputPassword.getText().toString();
+//
+//                try {
+//
+//                    if (password.length() > 0 && email.length() > 0) {
+//                        PD.show();
+//                        auth.signInWithEmailAndPassword(email, password)
+//                                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+//                                    @Override
+//                                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                                        if (!task.isSuccessful()) {
+//                                            Toast.makeText(
+//                                                    LoginActivity.this,
+//                                                    "Authentication Failed",
+//                                                    Toast.LENGTH_LONG).show();
+//                                            Log.v("error", task.getResult().toString());
+//                                        } else {
+//                                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                                            startActivity(intent);
+//                                            finish();
+//                                        }
+//                                        PD.dismiss();
+//                                    }
+//                                });
+//                    } else {
+//                        Toast.makeText(
+//                                LoginActivity.this,
+//                                "Fill All Fields",
+//                                Toast.LENGTH_LONG).show();
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+
+
+
+
+                Toast.makeText(
+                                                    LoginActivity.this,
+                                                    "logged in successfully",
+                                                    Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                            finish();
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override  public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.forget_password_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ForgetAndChangePasswordActivity.class).putExtra("Mode", 0));
+            }
+        });
+
+    }
+
+    @Override    protected void onResume() {
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+        super.onResume();
+    }
+}
