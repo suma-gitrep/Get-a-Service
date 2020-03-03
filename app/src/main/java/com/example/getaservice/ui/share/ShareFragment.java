809ab209d37d90f6f2757d0d88f91433f6e2b2b7
manideepchamala <54381062+manideepchamala@users.noreply.github.com>
2020-03-02 22:44:17 -0600

    i worked on forgetpassword and share fragment

---
+++ app/src/main/java/com/example/getaservice/ui/share/ShareFragment.java
@@ -0,0 +1,55 @@
+package com.example.getaservice.ui.share;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.getaservice.R;
+
+public class ShareFragment extends Fragment {
+
+    private ShareViewModel shareViewModel;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        shareViewModel =
+                ViewModelProviders.of(this).get(ShareViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_share, container, false);
+        final TextView textView = root.findViewById(R.id.text_share);
+
+        final Button share=(Button)root.findViewById(R.id.share);
+
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(getContext(),"Successfully you logged out of the app", Toast.LENGTH_LONG).show();
+
+                Intent shareIntent =   new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Insert Subject here");
+                String app_url = " https://play.google.com/store/apps/details?id=my.example.javatpoint";
+                shareIntent.putExtra(Intent.EXTRA_TEXT,app_url);
+                startActivity(Intent.createChooser(shareIntent, "Share via"));
+            }
+        });
+        shareViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+              //  textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
