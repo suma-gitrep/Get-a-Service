082eae6f9cb5fdb1734063807b484bd7dac63120
chetan <chetankudaravalli16@gmail.com>
2020-03-31 21:52:35 -0500

    customer register updated and commited

--- app/src/main/java/com/example/getaservice/CustomerRegisterActivity.java
+++ app/src/main/java/com/example/getaservice/CustomerRegisterActivity.java
@@ -77,6 +77,7 @@ public class CustomerRegisterActivity extends AppCompatActivity {
                                 });
                     }
 
+                    
                     else
                         {
                         Toast.makeText(
