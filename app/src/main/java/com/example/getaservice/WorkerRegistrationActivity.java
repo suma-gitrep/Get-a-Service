6c36dbc6a86a73b3af16fecaf1bacf4c3930558a
mohanpratapa <pvskrishnamohan24@gmail.com>
2020-03-31 20:54:08 -0500

    worker registration file graded

--- app/src/main/java/com/example/getaservice/WorkerRegistrationActivity.java
+++ app/src/main/java/com/example/getaservice/WorkerRegistrationActivity.java
@@ -19,6 +19,7 @@ Button sign_up_button;
 
     @Override    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_register_worker);
 
 
