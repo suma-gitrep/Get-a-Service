c2045291e85bb20927d1bb712de7990fc51d26b6
chetan <chetankudaravalli16@gmail.com>
2020-03-31 21:53:13 -0500

    file changed and commited

--- app/src/main/java/com/example/getaservice/CustomerRegisterActivity.java
+++ app/src/main/java/com/example/getaservice/CustomerRegisterActivity.java
@@ -77,7 +77,9 @@ public class CustomerRegisterActivity extends AppCompatActivity {
                                 });
                     }
 
+
                     
+
                     else
                         {
                         Toast.makeText(
