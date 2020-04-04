3db1a6ccfdce5fecc7d607e53c38a7a81fadc70a
mohanpratapa <pvskrishnamohan24@gmail.com>
2020-04-03 23:45:09 -0500

    Login activity Intent was modified

--- app/src/main/java/com/example/getaservice/LoginActivity.java
+++ app/src/main/java/com/example/getaservice/LoginActivity.java
@@ -48,7 +48,9 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
-               
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
 
 
 
