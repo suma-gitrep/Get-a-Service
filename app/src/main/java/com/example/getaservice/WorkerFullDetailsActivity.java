4db0c2b64c967527754f21f28b136690bb6642db
chetan <chetankudaravalli16@gmail.com>
2020-04-03 23:35:00 -0500

    worker full details commit

--- app/src/main/java/com/example/getaservice/WorkerFullDetailsActivity.java
+++ app/src/main/java/com/example/getaservice/WorkerFullDetailsActivity.java
@@ -6,13 +6,5 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class WorkerFullDetailsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workerdetails);
-        {
-
-
-        }
-    }
+    
 }
