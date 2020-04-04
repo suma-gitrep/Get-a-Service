974213e9a0323e9ae45b332658ecbb9b0c60d0ac
suma-gitrep <s537239@nwmissouri.edu>
2020-04-03 22:48:45 -0500

    worker registration sharedprefernce functionality

--- app/src/main/java/com/example/getaservice/WorkerRegistrationActivity.java
+++ app/src/main/java/com/example/getaservice/WorkerRegistrationActivity.java
@@ -1,12 +1,16 @@
 package com.example.getaservice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +19,10 @@ import java.util.List;
 
 public class WorkerRegistrationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 Button sign_up_button;
+
+EditText username,password,email,phonenumber,experience,certification,charges,address;
+SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
 
     @Override    protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +59,85 @@ Button sign_up_button;
         // attaching data adapter to spinner
         spinner.setAdapter(dataAdapter);
 
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        username=(EditText)findViewById(R.id.userName);
+        address=(EditText)findViewById(R.id.address);
+        phonenumber=(EditText)findViewById(R.id.phone);
+charges=(EditText)findViewById(R.id.charges);
+
+        experience = (EditText) findViewById(R.id.experience);
+        certification = (EditText) findViewById(R.id.certifications);
+
+
+
+        pref = getSharedPreferences("WorkerRegistration", 0);
+        // get editor to edit in file
+        editor = pref.edit();
         sign_up_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(WorkerRegistrationActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
+
+
+
+                final String emailstr = email.getText().toString();
+                final String passwordstr = password.getText().toString();
+                final String usernamestr= username.getText().toString();
+                final String chargestr=charges.getText().toString();
+
+                final String phonestr=phonenumber.getText().toString();
+                final String addressstr=address.getText().toString();
+                final String experiencestr=experience.getText().toString();
+                final String certificationstr=certification.getText().toString();
+
+
+                if(email.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                }
+                else if( password.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                }
+                else if( username.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter username", Toast.LENGTH_SHORT).show();
+                }
+                else if( charges.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter charges password", Toast.LENGTH_SHORT).show();
+                }
+                else if( phonenumber.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter phone number", Toast.LENGTH_SHORT).show();
+                }
+                else if( certification.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter phone number", Toast.LENGTH_SHORT).show();
+                }
+                else if( experience.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter phone number", Toast.LENGTH_SHORT).show();
+                }
+
+                else if( address.getText().length()<=0){
+                    Toast.makeText(WorkerRegistrationActivity.this, "Enter address details", Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    editor.putString("workerName", usernamestr);
+                    editor.putString("workerEmail",emailstr);
+                    editor.putString("workerPassword",passwordstr);
+                    editor.putString("workeraddress",addressstr);
+
+                    editor.putString("workercharges",chargestr);
+                    editor.putString("workerphone",phonestr);
+                    editor.putString("workerexperience",experiencestr);
+                    editor.putString("workercertification",certificationstr);
+
+                    editor.putString("wusertype","Worker");
+                    editor.commit();   // commit the values
+
+                    // after saving the value open next activity
+                    Intent ob = new Intent(WorkerRegistrationActivity.this, LoginActivity.class);
+                    startActivity(ob);
+                }
+
+
 
             }
         });
