ccb55826ba7421d252eb9441fb48263c8fb35484
suma-gitrep <s537239@nwmissouri.edu>
2020-04-03 19:50:23 -0500

    customer edit profile

--- app/src/main/java/com/example/getaservice/CustomerEditActivity.java
+++ app/src/main/java/com/example/getaservice/CustomerEditActivity.java
@@ -9,5 +9,7 @@ public class CustomerEditActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customereditprofile);
+
     }
 }
