cdd38e46aa9cb7a575d676f93f149c2fd85ab6ee
manideepchamala <54381062+manideepchamala@users.noreply.github.com>
2020-04-18 00:41:14 -0500

    Changes in dialog activity

---
+++ app/src/main/java/com/example/getaservice/ExampleDialog.java
@@ -0,0 +1,78 @@
+package com.example.getaservice;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class  ExampleDialog extends AppCompatDialogFragment {
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private ExampleDialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog, null);
+
+        builder.setView(view)
+                .setTitle("Book Now")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Toast toast = Toast.makeText(getActivity(),"Booking Cancelled"
+                                , Toast.LENGTH_SHORT);
+
+                        toast.show();
+
+                    }
+                })
+                .setPositiveButton("Book Now", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String username = editTextUsername.getText().toString();
+                        String password = editTextPassword.getText().toString();
+                        listener.applyTexts(username, password);
+                        Toast toast = Toast.makeText(getActivity(),"Booked Successfully"
+                                , Toast.LENGTH_SHORT);
+
+                        toast.show();
+
+                    }
+                });
+
+        editTextUsername = view.findViewById(R.id.edit_username);
+        editTextPassword = view.findViewById(R.id.edit_password);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+
+
+        try {
+            listener = (ExampleDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() +
+                    "must implement ExampleDialogListener");
+        }
+    }
+
+
+    public interface ExampleDialogListener {
+        void applyTexts(String username, String password);
+    }
+}
